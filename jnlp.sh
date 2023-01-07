763a661306f70bb344bf8250ee44dc1902fad5fe
Jon Krake <1024jon@gmail.com>
2023-01-06 17:32:24 -0700

    update to check file creation time and to only open every other cuz weid file system things

--- jnlp.sh
+++ jnlp.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 LASTFILE=none
+everyother=1
 inotifywait -m ~/Downloads -e close_nowrite |
     while read directory action file; do
         if [[ "$file" =~ .*jnlp$ ]]; then # Does the file end with .xml?
-            if [[ $LASTFILE != $file ]]; then
-              javaws ~/Downloads/"$file"
-              rm ~/Downloads/"$file"
-              LASTFILE=$file
+        filecreatedtime=$(stat -c '%w' ~/Downloads/$file)
+        echo $filecreatedtime
+            if [[ $LASTFILE != $filecreatedtime ]]; then
+              if [[ $everyother == 1 ]]; then
+                 javaws ~/Downloads/"$file"
+                 rm ~/Downloads/"$file"
+                 everyother=0
+              else
+                 everyother=1
+              fi
+              LASTFILE=$filecreatedtime
             fi
         fi
     done
